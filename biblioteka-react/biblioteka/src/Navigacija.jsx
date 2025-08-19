2075f9e666c84073a9fae795607725511a8dc39a
Jovana Gajic <jovanagajic09@gmail.com>
2025-08-19 20:57:14 +0200

    Dodaj izlogovanje

--- biblioteka-react/biblioteka/src/Navigacija.jsx
+++ biblioteka-react/biblioteka/src/Navigacija.jsx
@@ -2,11 +2,18 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useNavigate } from 'react-router';
 
 function BasicExample({}) {
   
   const loggedIn = localStorage.getItem("token") !== null;
 
+  const navigate = useNavigate();
+
+  const izloguj = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -23,7 +30,7 @@ function BasicExample({}) {
              {!loggedIn ? (
               <Nav.Link href="/login">Login</Nav.Link>
             ) : (
-              <Nav.Link href="/logout">Logout</Nav.Link>
+              <Nav.Link onClick={izloguj}>Logout</Nav.Link>
             )}
              
         
